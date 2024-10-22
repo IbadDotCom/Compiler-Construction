764f06628a14635bd06ce20b39d3a017ab143c67
Muhammad Ibad <72140583+IbadDotCom@users.noreply.github.com>
2024-10-22 20:47:31 +0500

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -16,25 +16,34 @@ TOKENS = [
 # Lexical Analyzer
 class LexicalAnalyzer:
     def __init__(self, source_code):
-        self.source_code = source_code
+        self.source_code = source_code.lstrip()
         self.tokens = []
-    
+        self.current_line = 1
+
     def tokenize(self):
         position = 0
         while position < len(self.source_code):
             match_found = False
+
+            if self.source_code[position] == '\n':
+                self.current_line += 1
+                position += 1
+                continue
+
             for token_type, pattern in TOKENS:
                 regex = re.compile(pattern)
                 match = regex.match(self.source_code, position)
                 if match:
                     lexeme = match.group(0)
-                    if token_type != 'WHITESPACE':  # Ignore whitespace
-                        self.tokens.append((token_type, lexeme))
+                    if token_type != 'WHITESPACE':
+                        self.tokens.append((token_type, lexeme, self.current_line))
                     position = match.end()
                     match_found = True
                     break
+
             if not match_found:
-                raise SyntaxError(f"Illegal character '{self.source_code[position]}' at position {position}")
+                raise SyntaxError(f"Illegal character '{self.source_code[position]}' at line {self.current_line}, position {position}")
+        
         return self.tokens
 
 if __name__ == '__main__':
